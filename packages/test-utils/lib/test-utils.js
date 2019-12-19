c370e852a7eecc7fd8a8ff6ae162e7b2f194b914
Tim Leslie <timl@thinkmill.com.au>
2019-12-19 18:01:15 +1100

    WIP

--- packages/test-utils/lib/test-utils.js
+++ packages/test-utils/lib/test-utils.js
@@ -27,7 +27,7 @@ async function setupServer({
   const argGenerator = {
     mongoose: getMongoMemoryServerConfig,
     knex: () => ({
-      dropDatabase: true,
+      initDatabase: true,
       knexOptions: {
         connection:
           process.env.DATABASE_URL || process.env.KNEX_URI || 'postgres://localhost/keystone',
